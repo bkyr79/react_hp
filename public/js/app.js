983b8c2abf94fdf65357461be7baeef86ff69f07
bkyr79 <cup_8479@yahoo.co.jp>
2022-05-08 23:05:45 +0900

    モーダル表示を修正（= モーダルを上から被せる見せ方にした）

--- public/js/app.js
+++ public/js/app.js
@@ -4462,20 +4462,67 @@ var __importDefault = this && this.__importDefault || function (mod) {
   \*******************************************************/
 /***/ (function(module, exports, __webpack_require__) {
 
-var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __importDefault = this && this.__importDefault || function (mod) {
+var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+var __createBinding = this && this.__createBinding || (Object.create ? function (o, m, k, k2) {
+  if (k2 === undefined) k2 = k;
+  Object.defineProperty(o, k2, {
+    enumerable: true,
+    get: function get() {
+      return m[k];
+    }
+  });
+} : function (o, m, k, k2) {
+  if (k2 === undefined) k2 = k;
+  o[k2] = m[k];
+});
+
+var __setModuleDefault = this && this.__setModuleDefault || (Object.create ? function (o, v) {
+  Object.defineProperty(o, "default", {
+    enumerable: true,
+    value: v
+  });
+} : function (o, v) {
+  o["default"] = v;
+});
+
+var __importStar = this && this.__importStar || function (mod) {
+  if (mod && mod.__esModule) return mod;
+  var result = {};
+  if (mod != null) for (var k in mod) {
+    if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+  }
+
+  __setModuleDefault(result, mod);
+
+  return result;
+};
+
+var __importDefault = this && this.__importDefault || function (mod) {
   return mod && mod.__esModule ? mod : {
     "default": mod
   };
 };
 
-!(__WEBPACK_AMD_DEFINE_ARRAY__ = [__webpack_require__, exports, __webpack_require__(/*! react */ "./node_modules/react/index.js"), __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/index.js"), __webpack_require__(/*! axios */ "./node_modules/axios/index.js")], __WEBPACK_AMD_DEFINE_RESULT__ = (function (require, exports, react_1, react_router_dom_1, axios_1) {
+!(__WEBPACK_AMD_DEFINE_ARRAY__ = [__webpack_require__, exports, __webpack_require__(/*! react */ "./node_modules/react/index.js"), __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/index.js"), __webpack_require__(/*! axios */ "./node_modules/axios/index.js"), __webpack_require__(/*! ./SpecifyReceivingDate */ "./resources/ts/component/reserve/SpecifyReceivingDate.tsx")], __WEBPACK_AMD_DEFINE_RESULT__ = (function (require, exports, react_1, react_router_dom_1, axios_1, SpecifyReceivingDate_1) {
   "use strict";
 
   Object.defineProperty(exports, "__esModule", ({
     value: true
   }));
-  react_1 = __importDefault(react_1);
+  react_1 = __importStar(react_1);
   axios_1 = __importDefault(axios_1);
+  SpecifyReceivingDate_1 = __importDefault(SpecifyReceivingDate_1);
 
   var MenuDetail = function MenuDetail() {
     var hamburg_steak = {
@@ -4507,6 +4554,19 @@ var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __importDefa
       return navigation('/specifyReceivingDate');
     };
 
+    var closeModal = function closeModal() {
+      setIsModalOpen(false);
+    };
+
+    var _ref = (0, react_1.useState)(false),
+        _ref2 = _slicedToArray(_ref, 2),
+        isModalOpen = _ref2[0],
+        setIsModalOpen = _ref2[1];
+
+    var openModal = function openModal() {
+      setIsModalOpen(true);
+    };
+
     return react_1["default"].createElement(react_1["default"].Fragment, null, react_1["default"].createElement("img", {
       src: "https://www.saizeriya.co.jp/menu/colorbox/images/photo03.jpg",
       alt: ""
@@ -4514,13 +4574,42 @@ var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __importDefa
       style: {
         marginRight: '2px'
       },
-      onClick: specifyReceivingDate
-    }, "\u6CE8\u6587\u3059\u308B"));
+      onClick: openModal
+    }, "\u6CE8\u6587\u3059\u308B"), isModalOpen ? react_1["default"].createElement(SpecifyReceivingDate_1["default"], {
+      onClick: function onClick() {
+        closeModal();
+      }
+    }) : "");
   };
 
   exports["default"] = MenuDetail;
 }).apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__),
-		__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+		__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__)); // // 親コンポーネント
+// export default function Modal_FunctionComponent(){
+// function closeModal(){
+//   setIsModalOpen(false)
+// }
+//   // -----2------
+//   const[isModalOpen,setIsModalOpen]=useState(false)
+//   // ---↑追加----
+//   // -----3------
+//   function openModal(){
+//     setIsModalOpen(true)
+//   }
+//   // ---↑追加----
+//   return (
+//     <div className="modalpage">
+//       <h2>関数コンポーネント</h2>
+//       // -----4------
+//       <button onClick={()=>{openModal()}}>モーダルを開く</button>
+//       // ---↑修正----
+//       // -----5------
+//       {isModalOpen? <Modal onClick={()=>{closeModal()}}/> :""}
+//       // ---↑修正----
+//     </div>
+//   );
+// }
+// }
 
 /***/ }),
 
--- resources/ts/component/reserve/MenuDetail.tsx
+++ resources/ts/component/reserve/MenuDetail.tsx
@@ -1,37 +1,86 @@
 import React, {useState, useCallback} from 'react'
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
+import SpecifyReceivingDate from "./SpecifyReceivingDate";
 
 
 const MenuDetail = (): JSX.Element => {
     
-    const hamburg_steak = { ordering_details88: '・ハンバーグステーキ' }
-    const pepe = { ordering_details88: '・ペペロンチーノ' }
-    const ice_cream = { ordering_details88: '・ジェラート' }
-    
-    const SendLineMessage = (order: any) => {
-        axios.post('/sendLineMessage', order)
-        .then(function (data) {        
-          console.log(data);
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
-    }
+  const hamburg_steak = { ordering_details88: '・ハンバーグステーキ' }
+  const pepe = { ordering_details88: '・ペペロンチーノ' }
+  const ice_cream = { ordering_details88: '・ジェラート' }
+  
+  const SendLineMessage = (order: any) => {
+      axios.post('/sendLineMessage', order)
+      .then(function (data) {        
+        console.log(data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
 
-    // 改善する必要あり！
-    const SendLineMessageH = () => SendLineMessage(hamburg_steak)
+  // 改善する必要あり！
+  const SendLineMessageH = () => SendLineMessage(hamburg_steak)
 
-    const navigation = useNavigate();
-    const specifyReceivingDate = () => navigation('/specifyReceivingDate');
+  const navigation = useNavigate();
+  const specifyReceivingDate = () => navigation('/specifyReceivingDate');
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
   return (
   <>
     <img src="https://www.saizeriya.co.jp/menu/colorbox/images/photo03.jpg" alt="" />
     <p>この商品の詳細です</p>
     {/* <button style={{marginRight:'2px'}} onClick={SendLineMessageH}>注文する</button> */}
-    <button style={{marginRight:'2px'}} onClick={specifyReceivingDate}>注文する</button>
+    <button style={{marginRight:'2px'}} onClick={openModal}>注文する</button>
+    
+    {/* <SpecifyReceivingDate /> */}
+    {/* @ts-ignore */}
+    {isModalOpen? <SpecifyReceivingDate onClick = {()=>{closeModal()}}/> :""}
+
   </>
   )
 }
 
 export default MenuDetail
+
+// // 親コンポーネント
+// export default function Modal_FunctionComponent(){
+
+  // function closeModal(){
+  //   setIsModalOpen(false)
+  // }
+
+//   // -----2------
+//   const[isModalOpen,setIsModalOpen]=useState(false)
+//   // ---↑追加----
+
+//   // -----3------
+//   function openModal(){
+//     setIsModalOpen(true)
+//   }
+//   // ---↑追加----
+
+//   return (
+//     <div className="modalpage">
+//       <h2>関数コンポーネント</h2>
+//       // -----4------
+//       <button onClick={()=>{openModal()}}>モーダルを開く</button>
+//       // ---↑修正----
+
+//       // -----5------
+//       {isModalOpen? <Modal onClick={()=>{closeModal()}}/> :""}
+//       // ---↑修正----
+//     </div>
+//   );
+// }
+// }
